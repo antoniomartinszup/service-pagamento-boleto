37f0f4beafde1389c7185bd9c32147230a7e7aa4
antoniomartinszup <antonio.martins@zup.com.br>
2021-11-01 10:37:17 -0300

    setup projeto

---
+++ src/main/java/br/com/zup/edu/pagamentoboleto/PagamentoBoletoApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.pagamentoboleto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagamentoBoletoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagamentoBoletoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/edu/pagamentoboleto/PagamentoBoletoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.pagamentoboleto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagamentoBoletoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
