c0331b912262984179b84b316fcf19e0bc5dde6f
Rolmer Telis de Oliveira Telis <rolmer.oliveira@zup.com.br>
2021-11-03 14:48:42 -0300

    inclusão do gerdor de boleto

---
+++ src/main/java/br/com/zup/edu/pagamentoboleto/antonio/Boleto.java
@@ -0,0 +1,55 @@
+package br.com.zup.edu.pagamentoboleto.antonio;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Boleto {
+
+    private Long id;
+    private Long numero;
+    private BigDecimal juroDeMoraDiario;
+    private Boolean permitePagametnoEmAtraso;
+    private LocalDate dataVencimento;
+    private LocalDate dataPagamento = null;
+    private BigDecimal diasEmAtraso = new BigDecimal(0.0);
+    private BigDecimal valorIncial;
+    private BigDecimal valorDosJuros = new BigDecimal(0.0);
+    private BigDecimal valorComJuros;
+
+    public Boleto(Long numero,
+                  BigDecimal juroDeMoraDiario,
+                  Boolean permitePagametnoEmAtraso,
+                  LocalDate dataVencimento,
+                  LocalDate dataPagamento,
+                  BigDecimal diasEmAtraso,
+                  BigDecimal valorIncial,
+                  BigDecimal valorDosJuros,
+                  BigDecimal valorComJuros) {
+        this.numero = numero;
+        this.juroDeMoraDiario = juroDeMoraDiario;
+        this.permitePagametnoEmAtraso = permitePagametnoEmAtraso;
+        this.dataVencimento = dataVencimento;
+        this.dataPagamento = dataPagamento;
+        this.diasEmAtraso = diasEmAtraso;
+        this.valorIncial = valorIncial;
+        this.valorDosJuros = valorDosJuros;
+        this.valorComJuros = valorComJuros;
+    }
+
+    @Override
+    public String toString() {
+        return "Boleto{" +
+                "id=" + id +
+                ", numero=" + numero +
+                ", juroDeMoraDiario=" + juroDeMoraDiario +
+                ", permitePagametnoEmAtraso=" + permitePagametnoEmAtraso +
+                ", dataVencimento=" + dataVencimento +
+                ", dataPagamento=" + dataPagamento +
+                ", diasEmAtraso=" + diasEmAtraso +
+                ", valorIncial=" + valorIncial +
+                ", valorDosJuros=" + valorDosJuros +
+                ", valorComJuros=" + valorComJuros +
+                '}';
+    }
+}
+
