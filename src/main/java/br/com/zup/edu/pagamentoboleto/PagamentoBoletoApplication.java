1443781db3714bfb091ac596f63e15c0fb3d8bfa
tales-zup <tales.araujo@zup.com.br>
2021-11-04 14:29:48 -0300

    removendo feign

--- src/main/java/br/com/zup/edu/pagamentoboleto/PagamentoBoletoApplication.java
+++ src/main/java/br/com/zup/edu/pagamentoboleto/PagamentoBoletoApplication.java
@@ -2,10 +2,8 @@ package br.com.zup.edu.pagamentoboleto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
 public class PagamentoBoletoApplication {
 
 	public static void main(String[] args) {
