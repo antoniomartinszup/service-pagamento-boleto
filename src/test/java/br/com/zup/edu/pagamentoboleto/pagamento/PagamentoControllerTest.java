673aff663c143556018ca981f4856bd5cff20b83
antoniomartinszup <antonio.martins@zup.com.br>
2021-11-03 23:50:46 -0300

    adicionando profile de test

--- src/test/java/br/com/zup/edu/pagamentoboleto/pagamento/PagamentoControllerTest.java
+++ src/test/java/br/com/zup/edu/pagamentoboleto/pagamento/PagamentoControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -19,6 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @AutoConfigureMockMvc
 @SpringBootTest
+@ActiveProfiles("test")
 public class PagamentoControllerTest {
 
     @Autowired
